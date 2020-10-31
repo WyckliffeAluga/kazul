7fd0d29f3ad4d5f550fe22302552f97393b29703
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-10-30 21:03:59 -0700

    proof of work system with hex

--- blockchain.js
+++ blockchain.js
@@ -35,10 +35,10 @@ class Blockchain {
     };
 
     for (let i =1; i < chain.length; i++ ) {
-      const {timestamp, lastHash, hash, data}  = chain[i];
+      const {timestamp, lastHash, hash, nonce, difficulty, data}  = chain[i];
       const actualLastHash = chain[i-1].hash;
       if (lastHash !== actualLastHash) return false;
-      const validatedHash = crytoHash(timestamp, lastHash, data);
+      const validatedHash = crytoHash(timestamp, lastHash, data, nonce, difficulty);
       if (hash !== validatedHash) return false;
     }
     return true;
