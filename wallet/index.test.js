53b38e3411af73fa079d07da4420f7266eb8e2a5
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-10-31 14:10:20 -0700

    wallet and signatures

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 
 const Wallet = require('./index') ;
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
   let wallet;
@@ -16,4 +17,25 @@ describe('Wallet', () => {
     expect(wallet).toHaveProperty('publicKey');
   });
 
+  describe('signing data', () => {
+    const data = 'Wyckliffe' ;
+
+    it('verifies a signature',() => {
+      expect(verifySignature({
+        publicKey: wallet.publicKey,
+        data,
+        signature: wallet.sign(data)
+      })).toBe(true);
+    });
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
+    });
+  });
 });
