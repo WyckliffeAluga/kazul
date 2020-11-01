93ed951ba1443a2b97c6d5f2bdd354386ebf8706
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-10-31 19:53:34 -0700

    transaction pool finished

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain  = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain , transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -43,6 +43,9 @@ app.post('/api/transact', (req, res) => {
   }
 
   transactionPool.setTransaction(transaction);
+
+  pubsub.broadcastTransaction(transaction);
+
   res.json({ type: 'success', transaction } );
 });
 
@@ -50,7 +53,7 @@ app.get('/api/transaction-pool-map', (req, res) => {
   res.json(transactionPool.transactionMap);
 });
 
-const SyncChains = () => {
+const syncWithRootState = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       const rootChain = JSON.parse(body);
@@ -59,7 +62,17 @@ const SyncChains = () => {
       blockchain.replaceChain(rootChain);
     }
   });
-}
+
+  request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootTransactionPoolMap = JSON.parse(body);
+
+      console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+      transactionPool.setMap(rootTransactionPoolMap);
+    }
+  });
+};
+
 let PEER_PORT ;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -70,6 +83,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`)
   if (PORT !== DEFAULT_PORT) {
-    SyncChains();
+    syncWithRootState();
   }
 });
