e06c9cd32d16f3a43a8300973ec850573d95e164
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-11-01 19:51:32 -0800

    more changes

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/api/wallet-info', (req, res) => {
   });
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
 
